0466ba6977eac45ef7d4adea0267bdab92eb168c
Nicholas Galen <galennicholas@proton.me>
2025-06-24 15:29:26 -0400

    Create exercise1.java

---
+++ src/main/java/LinkedLists/exercise1.java
@@ -0,0 +1,66 @@
+package LinkedLists;
+
+class Exercise1 {
+
+    private Node head;
+    private Node tail;
+    private int length;
+
+    class Node {
+        int value;
+        Node next;
+
+        Node(int value) {
+            this.value = value;
+        }
+    }
+
+
+    Exercise1(int value){
+        Node node = new Node(value);
+        head = node;
+        tail = node;
+        length = 1;
+    }
+
+
+    public Node getHead() {
+        return head;
+    }
+
+    public Node getTail() {
+        return tail;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public void printList() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.println(temp.value);
+            temp = temp.next;
+        }
+    }
+
+    public void printAll() {
+        if (length == 0) {
+            System.out.println("Head: null");
+            System.out.println("Tail: null");
+        } else {
+            System.out.println("Head: " + head.value);
+            System.out.println("Tail: " + tail.value);
+        }
+        System.out.println("Length:" + length);
+        System.out.println("\nLinked List:");
+        if (length == 0) {
+            System.out.println("empty");
+        } else {
+            printList();
+        }
+    }
+
+}
+
+
