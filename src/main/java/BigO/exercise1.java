7b9ced998e9b9462ef9848ec486d6707f6896983
Nicholas Galen <galennicholas@proton.me>
2025-06-24 08:58:28 -0400

    Initial commit

---
+++ src/main/java/BigO/exercise1.java
@@ -0,0 +1,18 @@
+package BigO;
+
+public class exercise1 {
+
+    public static void printItems(int n) {
+        for (int i = 0; i < n; i++ ){
+            System.out.println(i);
+        }
+    }
+
+
+
+    // DO NOT CHANGE THE MAIN METHOD BELOW
+    public static void main(String[] args) {
+        printItems(10);
+    }
+
+}
