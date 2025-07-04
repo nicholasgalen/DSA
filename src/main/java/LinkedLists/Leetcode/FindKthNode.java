7930ff3f7f7c4c0f5b5a0f33ae2b1f2fa55adadb
Nicholas Galen <galennicholas@proton.me>
2025-07-04 14:47:52 -0400

    sus >:(

---
+++ src/main/java/LinkedLists/Leetcode/FindKthNode.java
@@ -0,0 +1,94 @@
+package LinkedLists.Leetcode;
+
+public class FindKthNode {
+
+    private Node head;
+    private Node tail;
+
+    class Node {
+        int value;
+        Node next;
+
+        Node(int value) {
+            this.value = value;
+        }
+    }
+
+    public FindKthNode(int value) {
+        Node newNode = new Node(value);
+        head = newNode;
+        tail = newNode;
+    }
+
+    public Node getHead() {
+        return head;
+    }
+
+    public Node getTail() {
+        return tail;
+    }
+
+    public void printList() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.println(temp.value);
+            temp = temp.next;
+        }
+    }
+
+    public void printAll() {
+        if (head == null) {
+            System.out.println("Head: null");
+            System.out.println("Tail: null");
+        } else {
+            System.out.println("Head: " + head.value);
+            System.out.println("Tail: " + tail.value);
+        }
+        System.out.println("\nLinked List:");
+        if (head == null) {
+            System.out.println("empty");
+        } else {
+            printList();
+        }
+    }
+
+    public void makeEmpty() {
+        head = null;
+        tail = null;
+    }
+
+    public void append(int value) {
+        Node newNode = new Node(value);
+        if (head == null) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            tail.next = newNode;
+            tail = newNode;
+        }
+    }
+
+    public Node findKthFromEnd(int n){
+        Node slow = head;
+        Node fast = head;
+        int counter = 0;
+
+        while (fast != null){
+            fast = fast.next;
+            counter++;
+        }
+        if (counter < n){
+            return null;
+        }
+
+        while( n < counter){
+            assert slow != null;
+            slow = slow.next;
+            n++;
+        }
+
+        return slow;
+    }
+
+}
+
