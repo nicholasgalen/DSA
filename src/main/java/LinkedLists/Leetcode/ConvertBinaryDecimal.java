88fa8d6da8e45efa90a2b743df14c5c385cb7326
Nicholas Galen <galennicholas@proton.me>
2025-07-05 15:54:36 -0400

    Create ConvertBinaryDecimal.java

---
+++ src/main/java/LinkedLists/Leetcode/ConvertBinaryDecimal.java
@@ -0,0 +1,78 @@
+package LinkedLists.Leetcode;
+
+// Leetcode 1290. Convert Binary Number in a Linked List to Integer
+public class ConvertBinaryDecimal {
+
+    private Node head;
+    private int length;
+
+    class Node {
+        int value;
+        Node next;
+
+        Node(int value) {
+            this.value = value;
+        }
+    }
+
+    public ConvertBinaryDecimal(int value) {
+        Node newNode = new Node(value);
+        head = newNode;
+    }
+
+    public Node getHead() {
+        return head;
+    }
+
+    public void printList() {
+        Node temp = head;
+        if (temp == null) {
+            System.out.println("empty");
+        } else {
+            while (temp != null) {
+                System.out.print(temp.value);
+                temp = temp.next;
+                if (temp != null) {
+                    System.out.print(" -> ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    public void makeEmpty() {
+        head = null;
+        length = 0;
+    }
+
+    public void append(int value) {
+        Node newNode = new Node(value);
+        if (head == null) {
+            head = newNode;
+        } else {
+            Node current = head;
+            while (current.next != null) {
+                current = current.next;
+            }
+            current.next = newNode;
+        }
+    }
+
+    public int binaryToDecimal(){
+        Node current = head;
+        int sum = 0;
+
+        while (current != null){
+            sum *= 2;
+            if(current.value == 1){
+                sum++;
+            }
+            current = current.next;
+        }
+
+        return sum;
+    }
+}
+
+
+
